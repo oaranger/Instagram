f3551e6300b4f51fb22a807b7fa7df771b798f18
Binh Huynh <bin.huy@gmail.com>
2019-01-05 10:49:37 -0800

    Initial Commit

---
+++ FirebaseInstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseInstagram
+//
+//  Created by Binh Huynh on 1/5/19.
+//  Copyright © 2019 Binh Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
